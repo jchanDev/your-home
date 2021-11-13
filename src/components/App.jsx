7aa6843556eeaea594a594fed7476342ac562aa7
Ritu Kirsur <ritukirsur@169-231-174-59.wireless.ucsb.edu>
2021-11-13 14:36:09 -0800

    moved books into bookshelf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,42 +1,15 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import Book from "./Book";
-import CreateBook from "./CreateBook";
 import Bookshelf from "./Bookshelf";
+import CreateBook from "./CreateBook";
+import Book from "./Book";
 
 function App() {
-    const [books, setbooks] = useState([]);
-
-    function addBook(newBook) {
-        setbooks(prevBooks => {
-            return [...prevBooks, newBook];
-        });
-    }
-
-    function deleteBook(id) {
-        setbooks(prevBooks => {
-            return prevBooks.filter((noteItem, index) => {
-                return index !== id;
-            });
-        });
-    }
 
     return(
         <div>
             <Header />
-            <CreateBook onAdd={addBook} />
-            {books.map((bookItem, index) => {
-                return (
-                    <Book   
-                        key={index}
-                        id={index}
-                        title={bookItem.title}
-                        content={bookItem.content}
-                        onDelete={deleteBook}
-                    />
-                );
-            })}
             <Bookshelf />
             <Footer />
         </div>
--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,7 +1,43 @@
-import React from "react";
+
+import React, { useState } from "react";
+import CreateBook from "./CreateBook";
+import Book from "./Book";
 
 function Bookshelf() {
-    return <img src="./images/empty-bookshelf.png" class="bookshelf" alt="bookshelf" />
+    const [books, setbooks] = useState([]);
+
+    function addBook(newBook) {
+        setbooks(prevBooks => {
+            return [...prevBooks, newBook];
+        })
+    }
+
+    function deleteBook(id) {
+        setbooks(prevBooks => {
+            return prevBooks.filter((noteItem, index) => {
+                return index !== id;
+            });
+        });
+    }
+
+    return (
+    <div>
+    <img src="./images/empty-bookshelf.png" class="bookshelf" alt="bookshelf" />
+    
+    <CreateBook onAdd={addBook} />
+            {books.map((bookItem, index) => {
+                return (
+                    <Book   
+                        key={index}
+                        id={index}
+                        title={bookItem.title}
+                        content={bookItem.content}
+                        onDelete={deleteBook}
+                    />
+                );
+            })}
+
+    </div> );
 
 }
 
