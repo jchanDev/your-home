69e936686547649738687666ffefa6deb94fa4ae
Christine Tu <62481833+christine2515@users.noreply.github.com>
2021-11-13 14:43:47 -0800

    books onto bookshelf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,37 +6,12 @@ import CreateBook from "./CreateBook";
 import Bookshelf from "./Bookshelf";
 
 function App() {
-    const [books, setbooks] = useState([]);
-
-    function addBook(newBook) {
-        setbooks(prevBooks => {
-            return [...prevBooks, newBook];
-        });
-    }
-
-    function deleteBook(id) {
-        setbooks(prevBooks => {
-            return prevBooks.filter((noteItem, index) => {
-                return index !== id;
-            });
-        });
-    }
+    
 
     return(
         <div>
             <Header />
-            <CreateBook onAdd={addBook} />
-        {books.map((bookItem, index) => {
-            return (
-                <Book   
-                    key={index}
-                    id={index}
-                    title={bookItem.title}
-                    content={bookItem.content}
-                    onDelete={deleteBook}
-                />  
-            );
-        })}
+            
             <Bookshelf />
             <Footer />
         </div>
